5b86cafe17d635bf9b27111165db87f77879046c
tiantianttd <tidin@microsoft.com>
2025-08-24 22:24:18 -0700

    Fix: qualify UnityEngine.UI.CanvasScaler usages to avoid local CanvasScaler name collision

--- Assets/Scripts/UI/ResponsiveCanvasScaler.cs
+++ Assets/Scripts/UI/ResponsiveCanvasScaler.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(CanvasScaler))]
+[RequireComponent(typeof(UnityEngine.UI.CanvasScaler))]
 public class ResponsiveCanvasScaler : MonoBehaviour
 {
-    private CanvasScaler scaler;
+    private UnityEngine.UI.CanvasScaler scaler;
     private float defaultResolutionWidth = 1920f;
     private float defaultResolutionHeight = 1080f;
     private float targetAspectRatio = 16f / 9f;
 
     void Start()
     {
-        scaler = GetComponent<CanvasScaler>();
+    scaler = GetComponent<UnityEngine.UI.CanvasScaler>();
         UpdateCanvasScaling();
     }
 
@@ -24,7 +24,7 @@ public class ResponsiveCanvasScaler : MonoBehaviour
             // Screen is wider than target - use height as reference
             float scaleFactor = Screen.height / defaultResolutionHeight;
             scaler.referenceResolution = new Vector2(defaultResolutionWidth, defaultResolutionHeight);
-            scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+            scaler.screenMatchMode = UnityEngine.UI.CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
             scaler.matchWidthOrHeight = 1f; // Match height
         }
         else
@@ -32,7 +32,7 @@ public class ResponsiveCanvasScaler : MonoBehaviour
             // Screen is taller than target - use width as reference
             float scaleFactor = Screen.width / defaultResolutionWidth;
             scaler.referenceResolution = new Vector2(defaultResolutionWidth, defaultResolutionHeight);
-            scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+            scaler.screenMatchMode = UnityEngine.UI.CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
             scaler.matchWidthOrHeight = 0f; // Match width
         }
     }
